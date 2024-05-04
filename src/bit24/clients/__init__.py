8812560454a35b593bc2e866d8adf09dfffa5ba8
AMiWR <amiwrpremium@gmail.com>
2024-05-04 13:35:38 +0330

    feat(clients): Add abstract client class for main client implementation
    - Added `AbstractClient` class, which contains abstract methods for handling API requests.
    - This class serves as a blueprint for implementing main client classes.
    - Abstract methods include initialization of session, making requests, handling responses, and various API endpoints.

---
+++ src/bit24/clients/__init__.py
@@ -0,0 +1,10 @@
+"""
+# Clients
+
+This package contains the clients for the different services that the application uses.
+
+## Modules
+
+* [Core](_core) - Core client.
+* [Abstract](_abstract) - Abstract client.
+"""
