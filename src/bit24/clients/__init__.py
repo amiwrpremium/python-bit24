201b5815a5a5ed552b6f1e74baf9233a4bda0733
AMiWR <amiwrpremium@gmail.com>
2024-05-04 13:37:08 +0330

    feat(clients): Add main client class for interacting with the API
    - Added `Client` class, which serves as the main client for interacting with the API.
    - Inherits from `AbstractClient` and `CoreClient`.
    - Implements methods for making requests to the API endpoints.
    - Provides methods for interacting with various API functionalities such as withdrawal, deposit, order creation, etc.

--- src/bit24/clients/__init__.py
+++ src/bit24/clients/__init__.py
@@ -7,4 +7,5 @@ This package contains the clients for the different services that the applicatio
 
 * [Core](_core) - Core client.
 * [Abstract](_abstract) - Abstract client.
+* [Sync Client](_sync_client) - Sync client (use [requests](https://docs.python-requests.org/en/master/)).
 """
